b2474888730a22bc9ac35c2dbd93bcebefc97f91
riju.02 <mukherjee.rijuraj@gmail.com>
2022-09-13 22:20:30 +0530

    DSA Lab

--- C/THIRD SEMESTER/DSA Lab Day 6(07.09.2022)/Experminent4Q1.c
+++ C/THIRD SEMESTER/DSA Lab Day 6(07.09.2022)/Experminent4Q1.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 
-int main()
-{
-    int m, n;
-    printf("Enter the order of the matix \n");
-    printf("Enter the number of rows in array: ");
-    scanf("%d", &m);
-    printf("Enter the number of columns in array: ");
-    scanf("%d", &n);
-    int counter = 0;
+void display(int m, int n, int array[m][n]){
+    for(int i=0;i<m;++i){
+        for(int j=0;j<n;++j){
+            printf("%d ",array[i][j]);
+        }
+        printf("\n");
+    }
+}
 
-    printf("Enter the co-efficients of the matix: \n");
-    int array[m][n];
+int take_input(int n, int m, int array[n][m]){
+    int counter=0;
     for (int i = 0; i < m; ++i)
     {
         for (int j = 0; j < n; ++j)
@@ -23,7 +22,10 @@ int main()
             }
         }
     }
+    return counter;
+}
 
+void sparx(int m, int n, int counter){
     if (counter > ((m * n) / 2))
     {
         printf("The given matrix is sparse matrix \n");
@@ -31,4 +33,23 @@ int main()
     else
         printf("The given matrix is not a sparse matrix \n");
     printf("There are %d number of zeros", counter);
+}
+
+int main()
+{
+    int m, n;
+    printf("Enter the order of the matix \n");
+    printf("Enter the number of rows in array: ");
+    scanf("%d", &m);
+    printf("Enter the number of columns in array: ");
+    scanf("%d", &n);
+    int counter = 0;
+
+    printf("Enter the elements of the matix: \n");
+    int array[m][n];
+    counter=take_input(n, m, array);
+    printf("\n The Matrix is- \n");
+    display(m,n,array);
+    sparx(m, n, counter); 
+    return 0;
 }
